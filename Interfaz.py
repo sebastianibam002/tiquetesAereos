bad9c0e7059eeb8f7a7c36e1278d7b26cba84b3d
sebastianibam002 <55999978+sebastianibam002@users.noreply.github.com>
2019-10-28 13:02:52 -0500

    Update Interfaz.py

--- Interfaz.py
+++ Interfaz.py
@@ -1,36 +1,39 @@
+
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 import numpy as np
  
 ####################################### Ventana #############################################
+""" Se crea la ventana con su respectivo titulo """
 
 root = tk.Tk()
 root.wm_title ("Analisis de vuelo")
 
-
 ####################### Grafico (averiguar el tipo de grafico) ##############################
 
-fig = Figure (figsize = (5, 4), dpi=150)                                        #Tamaño
-t = np.arange(0,3, 0.01)                                                        # Rango
-fig.add_subplot(111).plot(t,2*np.sin(2*np.pi*t))                                # "Ecuación"
-canvas = FigureCanvasTkAgg (fig, master = root )                                # Insertar en Ventana
-canvas.draw()                                                                   # Diduja el Grafico
-canvas.get_tk_widget().pack(side= tk.TOP, fill=tk.BOTH, expand = 1)             # Ubicación
+fig = Figure (figsize = (5, 4), dpi=150)    # dimenciones de la figura ((ancho,alto), escala)---------#Tamaño
+t = np.arange(0,3, 0.01)   #intervalo de 0 a 3 en el eje X, con pasos de 0.01  ---------------------- # Rango
+fig.add_subplot(111).plot(t,2*np.sin(2*np.pi*t))  #-------------------------------------------------- #"Ecuación"
+canvas = FigureCanvasTkAgg (fig, master = root )  #-------------------------------------------------- #Insertar en Ventana
+canvas.draw() #---------------------------------------------------------------------------------------# Diduja el Grafico
+canvas.get_tk_widget().pack(side= tk.TOP, fill=tk.BOTH, expand = 1) # Se expande de acuerdo al tamaño de la ventana----# Ubicación
 
 ############################# Barra herramientas(OPCIONAL)################################### 
 
-toolbar = NavigationToolbar2Tk (canvas, root)
+toolbar = NavigationToolbar2Tk (canvas, root) #-------------------------------------------------------# Insertar en Ventana
 toolbar.update()
-canvas.get_tk_widget().pack(side= tk.TOP, fill=tk.BOTH, expand = 1)
+canvas.get_tk_widget().pack(side= tk.TOP, fill=tk.BOTH, expand = 1) # Se expande de acuerdo al tamaño de la ventana-------# Ubicación
 
-######################################## Boton ##############################################
+######################################## Botón ##############################################
 
-def cerrarVentana():
+"""Se define la funcion que cierra la ventana para luego vincularla a el botón 'Salir' """
+
+def CerrarVentana():
     root.destroy ()
     exit()
-boton = tk.Button(root, text = "Salir",command=cerrarVentana, bg = "red", fg = "white" )
-boton.pack(side = tk.BOTTOM)
+boton = tk.Button(root, text = "Salir",command = CerrarVentana, bg = "red", fg = "white") # (texto, comando, color de fondo, color de letra)
+boton.pack(side = tk.BOTTOM)   # Ubicación (parte inferior)
 
 ######################################### END ###############################################
 
